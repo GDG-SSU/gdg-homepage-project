d9618d22a85998e46150f375e0d77b9fe7123802
Genus <Genus@Genusui-MacBook-Pro.local>
2016-01-30 17:28:33 +0900

    temp [공부용] forms and models

--- gdg_flask/controller.py
+++ gdg_flask/controller.py
@@ -1,5 +1,8 @@
-from flask import render_template
-from gdg_flask import app
+from flask import render_template, url_for, redirect, request
+from gdg_flask import app, db
+from gdg_flask.forms import UserForm
+
+from .models import UserDB
 
 
 @app.route('/')
@@ -7,10 +10,37 @@ def home():
     return render_template("gdg-article/home.html")
 
 
-
 @app.route('/helper')
 def helper():
     return render_template("gdg-article/help-desk/gdg-ssu-help.html")
+
+
+@app.route('/test')
+def test():
+    user = UserDB(user_id='youdskaj', user_pw='textpw1212')
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route('/test12')
+def test12():
+    user = UserDB.query.all()
+    # print(user)
+    # print(user[0].user_id)
+
+    return render_template("gdg-article/help-desk/temp_test.html", test_db=user)
+
+
+@app.route('/tempform', methods=['GET', 'POST'])
+def tempForm():
+    form = UserForm()
+    if request.method == 'POST' and form.validate():
+        print('forms ok')
+    else:
+        print('forms no')
+
+    return render_template("gdg-article/help-desk/temp_form.html", form=form)
 
 # @app.route('/temp12')
 # def temp12():
