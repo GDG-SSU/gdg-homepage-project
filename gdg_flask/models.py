09d5abb03f15d3ba38ac9ca25dee9aebece6100d
이준수 <ljs93kr@gmail.com>
2016-01-30 16:06:07 +0900

    db 연동 시작, UserDB 클래스 생성

--- gdg_flask/models.py
+++ gdg_flask/models.py
@@ -12,10 +12,17 @@ class BaseModel(db.Model):
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
                               onupdate=db.func.current_timestamp())
 
+class UserDB(BaseModel):
+    __tablename__= "user_table"
+    user_id = db.Column(db.String(30))
+    user_pw = db.Column(db.String(255))
+    permission = db.Column(db.SMALLINT(), default=1)
 
-class GdgHelpDesk(BaseModel):
-    # 유저로 저장할지 안정했다.
-    __tablename__ = "t_gdg_help_desk"
-    help_title = db.Column(db.String(200))
-    help_content = db.Column(db.Text())
-    author_address = db.Column(db.String(15))
+
+
+# class GdgHelpDesk(BaseModel):
+#     # 유저로 저장할지 안정했다.
+#     __tablename__ = "t_gdg_help_desk"
+#     help_title = db.Column(db.String(200))
+#     help_content = db.Column(db.Text())
+#     author_address = db.Column(db.String(15))
